fd1e0217f5a6fbe33cbca8956ad2248aaee8c2d3
kangwl <kangwl2009@163.com>
2015-07-11 12:33:23 +0800

    Signed-off-by: kangwl <kangwl2009@163.com>

---
+++ WebAppBootStrap/Account/Login.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using XK.Authentication;
+
+namespace WebAppBootStrap.Account {
+    public partial class Login : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            if (LoginLogic.HasLogin) {
+                Response.Redirect(LoginLogic.DefaultUrl);
+            }
+        }
+
+        public string RetutnUrl { get { return Request.QueryString["returl"]; } } 
+    }
+}
---
+++ WebAppBootStrap/ViewHandler/User.ashx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppBootStrap.ViewHandler {
+    /// <summary>
+    /// User 的摘要说明
+    /// </summary>
+    public class User : IHttpHandler {
+
+        public void ProcessRequest(HttpContext context) {
+            context.Response.ContentType = "text/plain";
+            DealRequest(context);
+            context.Response.End();
+        }
+
+        private void DealRequest(HttpContext context) {
+            XK.Core.ListJson.User usersJson = new XK.Core.ListJson.User(context.Request);
+            string json = usersJson.GetDataJson();
+            context.Response.Write(json);
+        }
+
+        public bool IsReusable {
+            get {
+                return false;
+            }
+        }
+    }
+}
