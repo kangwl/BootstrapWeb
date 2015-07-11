5dabe00cb0a8114c3d8abe1e0d6016e85569a993
kangwl <kangwl2009@163.com>
2015-07-11 17:31:38 +0800

    xg1

--- WebAppBootStrap/Account/Login.aspx.cs
+++ WebAppBootStrap/Account/Login.aspx.cs
@@ -11,29 +11,10 @@ namespace WebAppBootStrap.Account {
     public partial class Login : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
 
-            //bool success = User_Bll.CreateTable();
-            //if (success) {
-            Dictionary<string, dynamic> dic = new Dictionary<string, dynamic>();
-            dic.Add("Name","kangwl");
-            dic.Add("UserID","kangwl");
-            dic.Add("UserPassword","123456");
-            dic.Add("Age",12);
-            dic.Add("Sex",1);
-            dic.Add("MobilePhone","15869165656");
-            dic.Add("Email","kangwl2009@163.com");
-            dic.Add("AddDateTime",DateTime.Now);
-            dic.Add("UpdateDateTime",DateTime.Now);
-            dic.Add("UserType",1);
-            bool success = User_Bll.Insert(dic);
-            if (success) {
-                int count = User_Bll.GetRecordCount();
-                Response.Write(count);
-            }
-            //}
             //test
-            //if (LoginLogic.HasLogin) {
-            //    Response.Redirect(LoginLogic.DefaultUrl);
-            //}
+            if (LoginLogic.HasLogin) {
+                Response.Redirect(LoginLogic.DefaultUrl);
+            }
         }
 
         public string RetutnUrl { get { return Request.QueryString["returl"]; } } 
