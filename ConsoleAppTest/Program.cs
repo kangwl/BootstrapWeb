f2d0ab4d9a6d5d77cf356a72b60029eb58179ed5
kangwl <kangwl2009@163.com>
2015-07-11 12:26:10 +0800

    Signed-off-by: kangwl <kangwl2009@163.com>

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleAppTest {
+    class Program {
+        static void Main(string[] args) {
+            TestM testM = new TestM();
+            Console.WriteLine(testM.GetName("2"));
+            Console.Read();
+        }
+
+
+    }
+
+    public class TestM {
+        public string GetName(string act) {
+            if (act == "1") {
+                return "n1";
+            }
+            else if (act == "2") {
+                return "n2";
+            }
+            else if (act == "3") {
+                return "n3";
+            }
+            return "unknow";
+        }
+    }
+}
