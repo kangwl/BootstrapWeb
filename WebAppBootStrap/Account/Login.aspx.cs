6d91f9d5fa251d53557f065e0065600d489e887f
kangwl <kangwl2009@163.com>
2015-07-11 17:25:43 +0800

    xg

--- WebAppBootStrap/Account/Login.aspx.cs
+++ WebAppBootStrap/Account/Login.aspx.cs
@@ -13,8 +13,22 @@ namespace WebAppBootStrap.Account {
 
             //bool success = User_Bll.CreateTable();
             //if (success) {
+            Dictionary<string, dynamic> dic = new Dictionary<string, dynamic>();
+            dic.Add("Name","kangwl");
+            dic.Add("UserID","kangwl");
+            dic.Add("UserPassword","123456");
+            dic.Add("Age",12);
+            dic.Add("Sex",1);
+            dic.Add("MobilePhone","15869165656");
+            dic.Add("Email","kangwl2009@163.com");
+            dic.Add("AddDateTime",DateTime.Now);
+            dic.Add("UpdateDateTime",DateTime.Now);
+            dic.Add("UserType",1);
+            bool success = User_Bll.Insert(dic);
+            if (success) {
                 int count = User_Bll.GetRecordCount();
                 Response.Write(count);
+            }
             //}
             //test
             //if (LoginLogic.HasLogin) {
