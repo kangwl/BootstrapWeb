bd012b23b1ea0976861f35c3348ee21e351096db
kangwl <kangwl2009@163.com>
2015-07-11 17:18:31 +0800

    Login.aspx.cs

--- WebAppBootStrap/Account/Login.aspx.cs
+++ WebAppBootStrap/Account/Login.aspx.cs
@@ -11,11 +11,11 @@ namespace WebAppBootStrap.Account {
     public partial class Login : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
 
-            bool success = User_Bll.CreateTable();
-            if (success) {
+            //bool success = User_Bll.CreateTable();
+            //if (success) {
                 int count = User_Bll.GetRecordCount();
                 Response.Write(count);
-            }
+            //}
             //test
             //if (LoginLogic.HasLogin) {
             //    Response.Redirect(LoginLogic.DefaultUrl);
